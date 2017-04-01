73d2111ce27c5eb5f14d5dc6fe05104c9a7c6eaf
Wouter J <wouterjansen4@gmail.com>
2017-04-01 14:51:57 +0200

    Update rpilikeplatform.py

--- src/alexapi/device_platforms/rpilikeplatform.py
+++ src/alexapi/device_platforms/rpilikeplatform.py
@@ -1,13 +1,119 @@
 import time
 from abc import ABCMeta
 import logging
-import NeoPixel
 from neopixel import *
 from .baseplatform import BasePlatform
 
 logger = logging.getLogger(__name__)
-
 GPIO = None
+strip = Adafruit_NeoPixel(12, 18, 800000, 5, False, 255)
+
+def fadeIn(color):
+    strip.setBrightness(0)
+    strip.show()
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    for j in range(255):
+        strip.setBrightness(j)
+        strip.show()
+        time.sleep(2 / 1000.0)
+
+
+def fadeOut(color):
+    strip.setBrightness(255)
+    strip.show()
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+    for j in range(255, -1, -1):
+        strip.setBrightness(j)
+        strip.show()
+        time.sleep(2 / 1000.0)
+
+
+def errorRed():
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(0, 255, 0))
+    strip.show()
+
+
+class SetupGreen(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.done = False
+
+    def run(self):
+        while not self.done:
+            for q in range(3):
+                for i in range(0, strip.numPixels(), 3):
+                    strip.setPixelColor(i + q, Color(255, 0, 0))
+                strip.show()
+                print "test"
+                time.sleep(50 / 1000.0)
+                for i in range(0, strip.numPixels(), 3):
+                    strip.setPixelColor(i + q, 0)
+        turnOff()
+
+
+def rotateBlue():
+    for i in range(0, 11, 1):
+        strip.setPixelColor(i, Color(0, 0, 255))
+        if i + 1 > 11:
+            strip.setPixelColor(i + 1 - 12, Color(0, 0, 160))
+        else:
+            strip.setPixelColor(i + 1, Color(0, 0, 160))
+        if i + 2 > 11:
+            strip.setPixelColor(i + 2 - 12, Color(0, 0, 100))
+        else:
+            strip.setPixelColor(i + 2, Color(0, 0, 100))
+        if i + 3 > 11:
+            strip.setPixelColor(i + 3 - 12, Color(0, 0, 50))
+        else:
+            strip.setPixelColor(i + 3, Color(0, 0, 50))
+        if i + 4 > 11:
+            strip.setPixelColor(i + 4 - 12, Color(0, 0, 100))
+        else:
+            strip.setPixelColor(i + 4, Color(0, 0, 100))
+        if i + 5 > 11:
+            strip.setPixelColor(i + 5 - 12, Color(0, 0, 160))
+        else:
+            strip.setPixelColor(i + 5, Color(0, 0, 160))
+        if i + 6 > 11:
+            strip.setPixelColor(i + 6 - 12, Color(0, 0, 255))
+        else:
+            strip.setPixelColor(i + 6, Color(0, 0, 255))
+        if i + 7 > 11:
+            strip.setPixelColor(i + 7 - 12, Color(0, 0, 160))
+        else:
+            strip.setPixelColor(i + 7, Color(0, 0, 160))
+        if i + 8 > 11:
+            strip.setPixelColor(i + 8 - 12, Color(0, 0, 100))
+        else:
+            strip.setPixelColor(i + 8, Color(0, 0, 100))
+        if i + 9 > 11:
+            strip.setPixelColor(i + 9 - 12, Color(0, 0, 50))
+        else:
+            strip.setPixelColor(i + 9, Color(0, 0, 50))
+        if i + 10 > 11:
+            strip.setPixelColor(i + 10 - 12, Color(0, 0, 100))
+        else:
+            strip.setPixelColor(i + 10, Color(0, 0, 100))
+        if i + 11 > 11:
+            strip.setPixelColor(i + 11 - 12, Color(0, 0, 160))
+        else:
+            strip.setPixelColor(i + 11, Color(0, 0, 160))
+        strip.show()
+        time.sleep(0.2)
+
+
+def setupNeoPixel():
+    strip.begin()
+    
+
+def turnOff():
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, Color(0, 0, 0))
+        strip.setBrightness(255)
+    strip.show()
 
 
 class RPiLikePlatform(BasePlatform):
@@ -16,7 +122,6 @@ class RPiLikePlatform(BasePlatform):
 	def __init__(self, config, platform_name, p_GPIO):
 		
 		global GPIO
-		global NeoPixel
 		GPIO = p_GPIO
 		
 		super(RPiLikePlatform, self).__init__(config, platform_name)
@@ -30,22 +135,22 @@ class RPiLikePlatform(BasePlatform):
 		GPIO.setup(self._pconfig['plb_light'], GPIO.OUT)
 		GPIO.output(self._pconfig['rec_light'], GPIO.LOW)
 		GPIO.output(self._pconfig['plb_light'], GPIO.LOW)
-		NeoPixel.setupNeoPixel()
+		setupNeoPixel()
 
 	def indicate_failure(self):
 		logger.info("failure")
 		for _ in range(0, 5):
 			time.sleep(.1)
-			NeoPixel.errorRed()
+			errorRed()
 			time.sleep(.1)
-			NeoPixel.turnOff()
+			turnOff()
 
 	def indicate_success(self):
 		logger.info("success")
 		for _ in range(0, 5):
-			NeoPixel.errorRed()
+			errorRed()
 			time.sleep(.1)
-			NeoPixel.turnOff()
+			turnOff()
 
 	def after_setup(self, trigger_callback=None):
 
@@ -58,23 +163,23 @@ class RPiLikePlatform(BasePlatform):
 	def indicate_recording(self, state=True):
 		logger.info("recording")
 		if state is True:
-			NeoPixel.errorRed()
+			errorRed()
 		if state is False:
-			NeoPixel.turnOff()
+			turnOff()
 
 	def indicate_playback(self, state=True):
 		logger.info("recording")
 		if state is True:
-			NeoPixel.errorRed()
+			errorRed()
 		if state is False:
-			NeoPixel.turnOff()
+			turnOff()
 
 	def indicate_processing(self, state=True):
 		logger.info("recording")
 		if state is True:
-			NeoPixel.errorRed()
+			errorRed()
 		if state is False:
-			NeoPixel.turnOff()
+			turnOff()
 
 	def detect_button(self, channel=None): # pylint: disable=unused-argument
 		self.button_pressed = True
